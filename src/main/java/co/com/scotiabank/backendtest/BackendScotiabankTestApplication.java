8866b223c41be92358a41319c049e75ab0687386
Jean Lozano <michecoanexa52@gmail.com>
2024-11-22 21:34:43 -0500

    Start project and initial setup (db and swagger)

---
+++ src/main/java/co/com/scotiabank/backendtest/BackendScotiabankTestApplication.java
@@ -0,0 +1,13 @@
+package co.com.scotiabank.backendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendScotiabankTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendScotiabankTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/scotiabank/backendtest/BackendScotiabankTestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.scotiabank.backendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendScotiabankTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
